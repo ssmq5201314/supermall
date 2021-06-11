7d9a6263c97deed2e6b8947e7e8bdb70398c6eb5
ssmq5201314 <ssmq5201314@126.com>
2021-06-11 23:08:58 +0800

    '修改第一次'

---
+++ src/vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      // 别名配置
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'configs': '@/configs',
+        'views': '@/views',
+        'plugins': '@/plugins',
+      }
+    }
+  }
+}
